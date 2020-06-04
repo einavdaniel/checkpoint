13afcf4df2263ce45b806d20d8411992806f9e55
einavdaniel <einav.d911@gmail.com>
2020-06-04 17:49:27 +0300

    data file added, get function reading data from file and sends result

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const PORT = 4000;
 const HOST = 'localhost';
 
 var express = require('express');
+var fs = require('fs')
 var app = express();
 var cors = require('cors');
 var bodyParser = require('body-parser')
@@ -16,13 +17,33 @@ sayhey = function (req, res) {
     } catch (error) {
         res.status(500).send("Couldn't say hello :( " + error.message)
     }
+}
+getCount = function(req, res){
+    try {
+        res.send(require('./data/count.json').postCount.toString())
 
+    } catch (error) {
+        res.status(500).send("Could not retrive count :( " + error.message)
+    }
+}
+
+updateCount = function (req, res){
+    let newPostCount = require('./data/count.json').postCount;
+    newPostCount ++;
+    let jsonData = JSON.stringify({ postCount: newPostCount });
+    fs.writeFile("./data/count.json", jsonData, function (err) {
+        if (err) {
+            console.log(err);
+        }
+    });
 }
 
 // parse application/json
 app.use(bodyParser.json())
 
-app.get('/', sayhey);
+app.get('/hello', sayhey);
+app.get('/', getCount);
+app.post('/', updateCount);
 
 var server = app.listen(PORT, function () { });
 console.log(`Server is running at http://${HOST}:${PORT}/`)
