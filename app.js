dd750b15d5558be2c5d48898d104b20280153671
einavdaniel <einav.d911@gmail.com>
2020-06-07 12:26:16 +0300

    Server will initialize data file if not exist

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const PORT = 4000;
 const HOST = 'localhost';
+const DATA_PATH = './data/count.json'
 
 var express = require('express');
 var fs = require('fs')
@@ -8,13 +9,32 @@ var cors = require('cors');
 var bodyParser = require('body-parser')
 
 app.use(cors());
+app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
-extractCountFromJson  = function () {
-    let rawdata = fs.readFileSync('./data/count.json')
+// verify data file exists or create one
+initData = function () {
+    try {
+        if (!fs.existsSync(DATA_PATH)) {
+            //if no file exists, create one and set count to 0
+            let jsonData = JSON.stringify({ postCount: 0 });
+            fs.writeFile(DATA_PATH, jsonData, function (err) {
+                if (err) {
+                    console.error(err);
+                }
+            });
+        }
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+extractCountFromJson = function () {
+    let rawdata = fs.readFileSync(DATA_PATH)
     let jsonData = JSON.parse(rawdata)
     return jsonData.postCount;
 }
+
 getCount = function (req, res) {
     try {
         res.send(extractCountFromJson().toString())
@@ -28,23 +48,25 @@ updateCount = function (req, res) {
         let newPostCount = extractCountFromJson();
         newPostCount++;
         let jsonData = JSON.stringify({ postCount: newPostCount });
-        fs.writeFile("./data/count.json", jsonData, function (err) {
+        fs.writeFile(DATA_PATH, jsonData, function (err) {
             if (err) {
-                console.log(err);
+                console.error(err);
             }
         });
-        res.status(200).send("cound updated");
+        res.status(200).send("count updated");
     }
     catch (error) {
         res.status(500).send("Error in updating." + error.message)
     }
 }
 
-// parse application/json
-app.use(bodyParser.json())
-
+// set HTTP functions
 app.get('/', getCount);
 app.post('/', updateCount);
 
+// initialize datafile
+initData();
+
+// start server
 var server = app.listen(PORT, function () { });
 console.log(`Server is running at http://${HOST}:${PORT}/`)
